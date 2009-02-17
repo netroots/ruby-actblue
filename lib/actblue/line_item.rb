23ecee2d54f399617c91da1e9121e5fe963ad0a4
Kyle Shank <kyle.shank@gmail.com>
2009-02-16 17:15:48 -0800

    Modified LineItem to reflect possible attributes accordin to documentation.

--- lib/actblue/line_item.rb
+++ lib/actblue/line_item.rb
@@ -3,8 +3,8 @@ module ActBlue
   class LineItem < ActiveBlue
     
     XML_NAME = 'lineitem'
-    ATTRIBUTES = ['id','effective-at','visibility']
-    ELEMENTS = ['amount', 'fee', 'entity']
+    ATTRIBUTES = ['id','effective-at', 'status', 'visibility']
+    ELEMENTS = ['amount', 'fee', 'entity', 'aq-fee', 'premium-fee', 'processing-fee', 'jurisdiction']
     
   end
   
